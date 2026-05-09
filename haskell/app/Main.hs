d03d12466071edef96c866ef627a21e39e919aff
Stephen Onnen <stephen.onnen@gmail.com>
2026-05-08 22:58:26 -0500

    update routes

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,8 +1,5 @@
 import Db (openDb)
-import Domain.User
-import Helpers
-import Network.HTTP.Types (status201)
-import Repo.User qualified as UserRepo
+import Routes.User (userRoutes)
 import Web.Scotty
 
 main :: IO ()
@@ -11,25 +8,5 @@ main = do
   scotty 3000 $ do
     get "/health" $
       text "ok"
-
-    get "/users" $
-      liftIO (UserRepo.listUsers conn) >>= json
-
-    get "/users/:id" $ do
-      uid <- UserId <$> captureParam "id"
-      liftIO (UserRepo.getUser conn uid) >>= respondMaybe
-
-    post "/users" $ do
-      nu <- jsonData
-      u <- liftIO $ UserRepo.createUser conn nu
-      status status201 >> json u
-
-    put "/users/:id" $ do
-      uid <- UserId <$> captureParam "id"
-      nu <- jsonData
-      liftIO (UserRepo.updateUser conn uid nu) >>= respondMaybe
-
-    delete "/users/:id" $ do
-      uid <- UserId <$> captureParam "id"
-      liftIO (UserRepo.deleteUser conn uid) >>= respondFound
+    userRoutes conn
 
---
+++ haskell/app/Routes/User.hs
@@ -0,0 +1,31 @@
+module Routes.User (userRoutes) where
+
+import Database.SQLite.Simple (Connection)
+import Domain.User
+import Helpers
+import Network.HTTP.Types (status201)
+import Repo.User qualified as UserRepo
+import Web.Scotty
+
+userRoutes :: Connection -> ScottyM ()
+userRoutes conn = do
+  get "/users" $
+    liftIO (UserRepo.listUsers conn) >>= json
+
+  get "/users/:id" $ do
+    uid <- UserId <$> captureParam "id"
+    liftIO (UserRepo.getUser conn uid) >>= respondMaybe
+
+  post "/users" $ do
+    nu <- jsonData
+    u <- liftIO $ UserRepo.createUser conn nu
+    status status201 >> json u
+
+  put "/users/:id" $ do
+    uid <- UserId <$> captureParam "id"
+    nu <- jsonData
+    liftIO (UserRepo.updateUser conn uid nu) >>= respondMaybe
+
+  delete "/users/:id" $ do
+    uid <- UserId <$> captureParam "id"
+    liftIO (UserRepo.deleteUser conn uid) >>= respondFound
